0f3f956ce3954d85cd11d17274f54988b047def6
Yashika Bhandari <yashikabhandari@Yashikas-MacBook-Air.local>
2024-06-04 20:44:03 +0530

    idk

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+//import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
     <> 
    <Navbar title="Textutils22" abouttext="abouttextutils"/>
-   <div className="container my-3"><TextForm heading="enter your text to analyse"/></div>
+   {/*<div className="container my-3"><TextForm heading="enter your text to analyse"/></div>
    {/* div container ke andar likhne se centre mein aajayega hamare text area */}
+   <div className="container my -3"><About/></div>
+  
 
 
     </>
